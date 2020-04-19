7546e862ccab03a1fd7e857c72595c7d1e29475d
Cristian <cristian.daniel.medina@correounivalle.edu.co>
2020-04-19 16:29:40 -0500

    Login 0.1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
 const cors = require("cors");
+const pool = require("./db");
 
+
+dotenv.config();
 //Middleware
 app.use(cors());
 
 
-app.listen(5000,()=>{
-    console.log("running on 5000");
-  }) 
+
+const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+   console.log(`running on ${port}`);
+  }); 
